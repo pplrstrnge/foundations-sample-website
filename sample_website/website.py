122632c19754f0e5d40a3df35e3d9a72850220b8
can.turgay <can.turgay@code.berlin>
2021-02-24 01:45:46 +0100

    menu template first try

--- sample_website/website.py
+++ sample_website/website.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, Flask!</h1>'
-
+def menu():
+    return render_template('menu_template.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
