d3426acf39451aeb3ea98e7957d40cd3ff646730
can.turgay <can.turgay@code.berlin>
2021-02-24 02:06:26 +0100

    menu template fifth try

--- sample_website/website.py
+++ sample_website/website.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def menu():
-    return render_template('menu_template.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/first-page')
+def first_page():
+    return render_template('first-page.html', page_title="First Template")
+
+
+@app.route('/second-page')
+def second_page():
+    return render_template('second-page.html', page_title="Second Template")
+
+# add additonal pages here using a similar format as above
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="localhost", port=8080, debug=True)
