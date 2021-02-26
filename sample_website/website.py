2a6127dd621e6c03e02c83f228af84b92d725ee3
can.turgay <can.turgay@code.berlin>
2021-02-26 12:27:34 +0100

    menu try1

--- sample_website/website.py
+++ sample_website/website.py
@@ -14,6 +14,10 @@ def hello_world():
 @app.route('/first-page')
 def first_page():
     return render_template('first-page.html', page_title="First Template")
-    
+
+@app.route('/second-page')
+def second_page():
+    return render_template('second-page.html', page_title="Second Template")
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
