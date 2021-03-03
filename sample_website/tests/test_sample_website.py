24bf588865346c928b8a7eb230b04de9b9fee337
can.turgay <can.turgay@code.berlin>
2021-03-03 19:03:07 +0100

    tests

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -2,10 +2,4 @@ from sample_website.website import hello_world
 
 
 def test_sample_website():
-<<<<<<< HEAD
     assert hello_world() == '<h1>Hello, Flask!</h1>'
-||||||| bba0121
-    assert hello_world() == '<h1>Hello, Flask!</h1>'
-=======
-    assert hello_world() == '<h1>Hello, Class!</h1>'
->>>>>>> upstream/main
