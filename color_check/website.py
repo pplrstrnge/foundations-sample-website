546201da1679e3c088db09f4339248b064021501
can.turgay <can.turgay@code.berlin>
2021-03-04 15:44:41 +0100

    changed the location of the log file

--- color_check/website.py
+++ color_check/website.py
@@ -4,7 +4,7 @@ from color_check.controllers.get_color_code import get_color_code, get_color_nam
 import logging
 app = Flask(__name__)
 
-logging.basicConfig(filename='color_check.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='/tmp/color_check.log.txt', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 
 @app.route('/')
